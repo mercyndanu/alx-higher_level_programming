ed31dde0ff9724310b38c6956144b81e313c1e04
mercyndanu <mercynm01@gmail.com>
2023-05-17 11:02:12 -0700

    madatory tasks

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- hat creates the table id_not_null on your MySQL server.
+-- database name will be passed as an argument of the mysql command
+
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
